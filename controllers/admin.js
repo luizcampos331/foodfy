26b8473926cb192ffce73f8d0d7efd4ac7cdad44
luizcampos331 <luizcampos331@gmail.com>
2020-06-29 10:12:04 -0300

    Documentação dos código da área admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,37 +1,49 @@
+//Importando biblioteca File System para manipular arquivos
 const fs = require('fs');
+//Importando dados do data.jsob
 const data = require('../data.json');
 
 // === Método Index - Visualizar Página ===
 exports.index = function(req, res) {
+  //Retornando a página renderizada e passando os dados de todas as receitas
   return res.render('admin/index', { items: data.recipes })
 }
 
 // === Método Create - Visualizar página ===
 exports.create = function(req, res) {
+  //Retornando página de criação de receitas renderizada
   return res.render('admin/create')
 }
 
 // === Método Show - Visualizar página ===
 exports.show = function(req, res) {
+  //Recuperando o index da url
   const index = req.params.index;
 
+  /*Retornando página de detalhes de uma receita renderizada, passado dados
+  somente da receita correta, atravez de sua posição no array de dados*/
   return res.render('admin/show', { item: data.recipes[index] })
 }
 
 // === Método Edit - Visualizar página ===
 exports.edit = function(req, res) {
+  //Recuperando o index da url
   const index = req.params.index;
 
+  /*Retornando página de edição de uma receita renderizada, passado dados
+  somente da receita correta, atravez de sua posição no array de dados*/
   return res.render('admin/edit', { item: data.recipes[index] })
 }
 
 // === Método POST - Ação ===
 exports.post = function(req, res) {
-  //Variáveis recebendo um array com todos os campos do formulário
+  //Variável keys recebendo um array com todos os campos do formulário
   const keys = Object.keys(req.body);
 
   //Percorrendo o array keys
   for(let key of keys){
+    /*Verificando se keys já chegou no ultimo campo, como o ultimo campos não é
+    obrigatório, ele não precisa passar pelo próximo if */
     if(req.body[key] != req.body.information) {
       //Verificando a existencia de valores em cada uma das chaves do req.body
       if(req.body[key] == "")
@@ -39,16 +51,17 @@ exports.post = function(req, res) {
     }
   }
 
+  //Desestruturando os campos no formulário que vem no req.body
   let { image, title, author, ingredients, preparation, information } = req.body;
 
   //Pego o ultimo membro do array
   const lastRecipe= data.recipes[data.recipes.length - 1]
-  //Inicio o id com 1
+  //Inicio o id com 0 para servir para index do array
   let id = 0;
-  //Se lastMember não estiver vazio ele guarda o lastMember.id + 1 na variável id
+  //Se lastRecipe não estiver vazio ele guarda o lastRecipe.id + 1 na variável id
   if(lastRecipe) id = lastRecipe.id + 1;
 
-  //Adiciona um novo objetivo ao final do array de instructors
+  //Adiciona um novo objetivo ao final do array de recipes
   data.recipes.push({
     id,
     image,
@@ -64,8 +77,10 @@ exports.post = function(req, res) {
   //Convertendo req.body em json,
   //CallBack = não deixa o sistema travado esperando a finalização do processamento
   fs.writeFile('data.json', JSON.stringify(data, null, 2), function(error) {
+    //Caso de algo erro, a mensagem é retornada
     if(error) return res.send("Write file error!")
 
+    //Redireciona para a página de receitas (index)
     return res.redirect("/admin/recipes")
   });
 }
@@ -75,48 +90,57 @@ exports.put = function(req, res) {
   //Pega o id que está vindo no corpo da requisição
   const { id } = req.body;
 
-  let index = 0;
   
-  /*Se algum id da receita for igual ao id passado corpo da requisição
+  /*Se algum id de receita for igual ao id passado no corpo da requisição,
   os dados da receita serão guardados na variável foundRecipe */
-  const foundRecipe = data.recipes.find(function(recipe, foundIndex) {
-    if (id == recipe.id) {
-      index = foundIndex;
-      return true;
-    }
+  const foundRecipe = data.recipes.find(function(recipe) {
+      return id == recipe.id;
   });
 
   //Se não for achado nenhuma receita com id igual ao da url é retornada uma mensagem
   if(!foundRecipe) return res.send('Recipe not found!');
 
+  //Recipe guarda o objeto com os dados da receita atualizada
   const recipe = {
     ...foundRecipe,
     ...req.body,
     id: Number(req.body.id),
   }
 
-  data.recipes[index] = recipe;
+  //a receita do array recebe os novos valores
+  data.recipes[id] = recipe;
 
+  //escrevendo no arquivo data.json,
+  //Convertendo req.body em json,
+  //CallBack = não deixa o sistema travado esperando a finalização do processamento
   fs.writeFile('data.json', JSON.stringify(data, null, 2), function(error) {
     if(error) return res.send('Write error!');
 
+    //Redireciona para a página de detalhes da receita
     return res.redirect(`/admin/recipes/${id}`)
   })
 }
 
 // === Método DELETE - Ação ===
 exports.delete = function(req, res) {
+  //Pega o id que está vindo no corpo da requisição
   const { id } = req.body;
 
+  //Filtra os dados do data.json e guarda somente os que não forem igual ao id passado
   const filteredRecipe = data.recipes.filter(function(recipe) {
     return id != recipe.id;
   });
 
+  //Guarda os dados novamente no array de receitas
   data.recipes = filteredRecipe;
 
+  //escrevendo no arquivo data.json,
+  //Convertendo req.body em json,
+  //CallBack = não deixa o sistema travado esperando a finalização do processamento
   fs.writeFile('data.json', JSON.stringify(data, null, 2), function(error) {
     if(error) return res.send('Write error!');
 
+      //Redireciona para a página de receitas (index)
     return res.redirect('/admin/recipes');
   })
 }
--- public/scripts.js
+++ public/scripts.js
@@ -1,106 +1,119 @@
+// === Página de receitas ===
+//Guarda na variável recipes todos so elementos que tiverem a classe .recipe
 const recipes = document.querySelectorAll('.recipe');
-
+//Percorre todos od elementos de recipes
 for(let recipe of recipes) {
-  //addEventListener é um ouvidor de eventos, nesse caso o evento de click
+  //Fica ouvindo o click nos elementos dentro do recipe
   recipe.addEventListener("click", function() {
+    //Guarda o id do elemento recipe na variável id
     const id = recipe.getAttribute('id');
+    //Abre a página de detalhes da receita com base no id
     window.location.href = `/receitas/${id}`
   });
 }
 
 
 // === Página de detalhes da receita ===
+//Guarda num array todos os campos com as classes solicitadas
 const hide = [
   document.querySelector('.hide1'), 
   document.querySelector('.hide2'),
   document.querySelector('.hide3')
 ];
 
+//Guarda num array todos os campos com as classes solicitadas
 const maximize = [
   document.querySelector('.ingredients'),
   document.querySelector('.steps'),
   document.querySelector('.information')
 ];
 
-
-if(hide[0]){
-  console.log(hide)
-  hide[0].addEventListener('click', function() {
-
-    if(maximize[0].classList.contains('maximize')) 
-      maximize[0].classList.remove('maximize');
-    else
-      maximize[0].classList.add('maximize');
-    
-  });
-
-  hide[1].addEventListener('click', function() {
-
-    if(maximize[1].classList.contains('maximize'))
-      maximize[1].classList.remove('maximize');
-    else 
-      maximize[1].classList.add('maximize');
-
-  });
-
-  hide[2].addEventListener('click', function() {
-
-    if(maximize[2].classList.contains('maximize'))
-      maximize[2].classList.remove('maximize');
-    else
-      maximize[2].classList.add('maximize');
+/*Se o primeiro array não estiver vazio, entra no bloco de código, usado para não
+interferir com os código abaixo */
+if(hide[0]){ 
+  //Percorre todo o array hide
+  for(let i = 0; i < hide.length; i++) {
+    //Fica ouvindo o click nos elementos dentro do hide
+    hide[i].addEventListener('click', function() {
+      //Se a classe maximize ja existir na posição do array "maximize"
+      if(maximize[i].classList.contains('maximize')) 
+        //A classe é removida
+        maximize[i].classList.remove('maximize');
+      else
+        //Se não ela é adicionada
+        maximize[i].classList.add('maximize');
       
-  });
+    });
+  }
 }
 
+
 // === Página de criação de receita ===
-//Novo ingrediente
+//Novo ingrediente ===
 function addIngredient() {
+  //Guarda na variável ingredients a div com id "ingredients"
   const ingredients = document.querySelector("#ingredients");
+  //Guarda na variável fieldContainer todos as divs com a classe "ingredient"
   const fieldContainer = document.querySelectorAll(".ingredient");
 
-  // Realiza um clone do último ingrediente adicionado
+  // Guarda na variável newField um clone do ultimo input do array fieldContainer
   const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
 
-  // Não adiciona um novo input se o último tem um valor vazio
+  // Não adiciona uma nova div ingredient se o último input tem um valor vazio
   if (newField.children[0].value == "") return false;
 
-  // Deixa o valor do input vazio
+  // Deixa o valor do novo input da nova div ingredient vazio
   newField.children[0].value = "";
+  //Adiciona a nova div e seu input na div de ingredients
   ingredients.appendChild(newField);
 }
 
-document
-  .querySelector('.add-ingredient')
-  .addEventListener("click", addIngredient);
+//Guarda na variável ingredients o botão "adicionar novo ingrediente"
+const ingredients = document.querySelector('.add-ingredient')
 
-//Novo passo de preparo
+//Se a variável não estiver fazia, entra no if, estratégia para não ocorrerem erros
+if(ingredients)
+  //Monitória o click no botão citado acima e, caso aja click, adicona a funcition addIngredient
+  ingredients.addEventListener("click", addIngredient);
+
+//Novo passo de preparo ===
 function addPreparation() {
+  //Guarda na variável preparations a div com id "preparations"
   const preparations = document.querySelector("#preparations");
+  //Guarda na variável fieldContainer todos as divs com a classe "preparation"
   const fieldContainer = document.querySelectorAll(".preparation");
 
-  // Realiza um clone do último ingrediente adicionado
+  // Guarda na variável newField um clone do ultimo input do array fieldContainer
   const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
 
-  // Não adiciona um novo input se o último tem um valor vazio
+  // Não adiciona uma nova div preparation se o último input tem um valor vazio
   if (newField.children[0].value == "") return false;
 
-  // Deixa o valor do input vazio
+  // Deixa o valor do novo input da nova div preparation vazio
   newField.children[0].value = "";
+  //Adiciona a nova div e seu input na div de preparations
   preparations.appendChild(newField);
 }
 
-document
-  .querySelector('.add-preparation')
-  .addEventListener("click", addPreparation);
+//Guarda na variável preparation o botão "adicionar novo passo"
+const preparation = document.querySelector('.add-preparation');
+
+//Se a variável não estiver fazia, entra no if, estratégia para não ocorrerem erros
+if(preparation)
+  //Monitória o click no botão citado acima e, caso aja click, adicona a funcition addPreparation
+  preparation.addEventListener("click", addPreparation);
+
+
 
 // === Confiormação de delete ===
 //Guarda todo o form-delete na variavel formDelete
 const formDelete = document.querySelector('#form-delete')
 //Quando for gerado um evento de submit dentro do form-delete ele entra na function
-formDelete.addEventListener('submit', function(event) {
-  //Mostra um popup de confirmação da exclusão
-  const confirmation = confirm('Deseja deletar?')
-  //Se a confirmação for cancelada ele interrompe o submit do botão deletar
-  if(!confirmation) event.preventDefault()
-})
+if(formDelete){
+  formDelete.addEventListener('submit', function(event) {
+    //Mostra um popup de confirmação da exclusão
+    const confirmation = confirm('Deseja excluir esta receita?')
+    //Se a confirmação for cancelada ele interrompe o submit do botão deletar
+    if(!confirmation) event.preventDefault()
+  })
+}
